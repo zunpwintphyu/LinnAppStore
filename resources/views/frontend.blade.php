1b14d9b280b18f8521b5f86ea7ed09add1e1ab26
aungsoeoo <aungsoeoo.94s@gmail.com>
2020-03-06 11:26:15 +0630

    Update frontend.blade.php

--- resources/views/frontend.blade.php
+++ resources/views/frontend.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 
-    <title>Linn App Store Apk Download</title>
+    <title>Linn App Store</title>
 
     <link rel="icon" href="http://appstore.test/img/favicon.png" type="image/x-icon" />
     <link rel="shortcut icon" href="http://appstore.test/img/favicon.png" type="image/x-icon" />
